e70201d8c4b18489509dfa672fe1a3ef32b2ef0a
luca <luca.bisognin13@gmail.com>
2024-10-23 17:20:28 +0200

    compiti per 25/10/2024

--- progetto_campionato/src/Campionato.java
+++ progetto_campionato/src/Campionato.java
@@ -6,11 +6,16 @@ Luca Bisognin - 17/10/2024
 
 // import librerie esterne
 import java.io.File;
+import java.io.FileWriter;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Campionato {
-    int squadre_presenti;
+    public static final String RED = "\033[0;31m"; // colore rosso per gli avvisi
+    public static final String GREEN = "\033[0;32m";   // GREEN
+    public static final String RESET = "\033[0m";  // reset colore
+
+    int squadre_presenti, pos;
     final int TOTALE_SQUADRE = 20;
     // arrayList delle squadre dell'oggetto campionato creato nel main
     ArrayList<Squadra> teams = new ArrayList<>(TOTALE_SQUADRE);
@@ -34,7 +39,7 @@ public class Campionato {
         }
     }
 
-    // metodo per costruire la stringa da stampare
+    // case 2 -> toString per la stampa dell'elenco delle squadre
     public String toString() {
         int i=1; // indice lista
         StringBuilder s = new StringBuilder("\nSquadre nel campionato\n");
@@ -46,14 +51,14 @@ public class Campionato {
         return s.toString();
     }
 
-    // metodo per aggiungere squadre al campionato
+    // case 3 -> aggiungere squadre al campionato
     public void addTeam(Scanner scanner) {
         if (squadre_presenti+1 >= TOTALE_SQUADRE) {
-            System.out.println("Attenzione: numero massimo di squadre raggiunto");
+            System.out.println(RED + "Attenzione: numero massimo di squadre raggiunto\n" + RESET);
             return;
         }
 
-        // lettura della nuova squdra
+        // lettura della nuova squadra
         scanner.nextLine(); // pulizia buffer
         System.out.print("Nome nuova squadra: ");
         String nuovaSquadra = scanner.nextLine();
@@ -62,34 +67,94 @@ public class Campionato {
         squadre_presenti++;
         // aggiunta della nuova squadra all'array
         teams.add(squadre_presenti, new Squadra(nuovaSquadra));
-        System.out.println("Squadra " + nuovaSquadra + " aggiunta.");
+        System.out.println(GREEN + "Squadra " + nuovaSquadra + " aggiunta.\n" + RESET);
     }
 
-    // metodo per rimuovere ultima squadra dal campionato
+    // case 4 -> aggiunta squadra a determinata posizione
+    public void addTeamInPos(Scanner scanner) {
+        if (squadre_presenti+1 >= TOTALE_SQUADRE) {
+            System.out.println(RED + "Attenzione: numero massimo di squadre raggiunto\n" + RESET);
+            return;
+        }
+
+        // scelta posizione
+        System.out.print("Posizione in cui aggiungere la squadra: ");
+        pos = scanner.nextInt();
+        // lettura della nuova squadra
+        scanner.nextLine(); // pulizia buffer
+        System.out.print("Nome nuova squadra: ");
+        String nuovaSquadra = scanner.nextLine();
+
+        // incremento numero di squadre
+        squadre_presenti++;
+        // aggiunta della nuova squadra all'array
+        teams.add(pos-1, new Squadra(nuovaSquadra));
+        System.out.println(GREEN + "Squadra '" + nuovaSquadra + "' aggiunta.\n" + RESET);
+    }
+
+    // case 5 -> rimozione ultima squadra
     public void removeLastTeam() {
         if (squadre_presenti<0) {
-            System.out.println("Attenzione: nessuna squadra nel campionato.");
+            System.out.println(RED + "Attenzione: nessuna squadra nel campionato.\n" + RESET);
             return;
         }
 
         // rimozione dell'ultima squadra
-        System.out.println("Squadra " + teams.get(squadre_presenti) + " rimossa.");
+        System.out.println(GREEN + "Squadra '" + teams.get(squadre_presenti) + "' rimossa.\n" + RESET);
         teams.remove(squadre_presenti);
-        squadre_presenti--;
+        squadre_presenti--; // decremento numero squadre presenti
     }
 
-    // metodo per rimuovere una squadra a piacere
-    public void removeTeam(int pos) {
-        pos--; // decremento --> l'utente seleziona un valore avanti rispetto agli indici lista
+    // case 6 -> rimozione squadra a determinata posizione
+    public void removeTeam(Scanner scanner) {
         if (pos < 0 || pos >= TOTALE_SQUADRE-1) {
-            System.out.println("Attenzione: nessuna squadra nel campionato.");
+            System.out.println(RED + "Attenzione: nessuna squadra nel campionato.\n" + RESET);
             return;
         }
 
+        System.out.print("Numero squadra da rimuovere: ");
+        pos = scanner.nextInt();
         // rimozione squadra scelta
-        System.out.println("Squadra " + teams.get(pos) + " rimossa.");
-        teams.remove(pos);
+        System.out.println(GREEN + "Squadra '" + teams.get(pos-1) + "' rimossa.\n" + RESET);
+        teams.remove(pos-1);
         squadre_presenti--;
 
+    }
+
+    // case 7 -> rinomina determinata squadra
+    public void changeTeamName(Scanner scanner) {
+        // scelta posizione
+        System.out.print("Numero squadra di cui cambiare nome: ");
+        pos = scanner.nextInt();
+
+        // recupero nome squadra di cui cambiare nome
+        String oldName = teams.get(pos-1).getNome();
+
+        // lettura del nuovo nome
+        scanner.nextLine(); // pulizia buffer
+        System.out.print("Nuovo nome squadra: ");
+        String newName = scanner.nextLine();
+
+        // cambio nome squadra alla posizione 'pos-1'
+        teams.set(pos-1, new Squadra(newName));
+
+        System.out.println(GREEN + "Squadra '" + oldName + "' rinominata in '" + newName + "'.\n" + RESET);
+    }
+
+    // case 8 -> salvataggio modifiche
+    public void saveChanges() {
+        try {
+            // creazione oggetti writer per salvare le modifiche
+            FileWriter writeChanges = new FileWriter("progetto_campionato/data/serieA.csv");
+            // sovrascrittura del file csv che contiene l'elenco delle squadre
+            for (Squadra t : teams) {
+                writeChanges.write(t.toString() + "\n");
+            }
+            // chiusura del writer
+            writeChanges.close();
+        } catch (Exception ex) {
+            System.out.println("Errore: " + ex.getMessage());
+        }
+        System.out.println(GREEN + "Modifiche Salvate Correttamente\n" + RESET);
     }
 }
--- progetto_campionato/src/Main.java
+++ progetto_campionato/src/Main.java
@@ -10,16 +10,17 @@ public class Main  {
     public static void printMenu() {
         System.out.print("""
                \nMenù
-               ---------------------------------
-               | [0] Termina Programma         |
-               | [1] Stampa Menù               |
-               | [2] Stampa Squadre            |
-               | [3] Aggiungi Squadra          |
-               | [4] Rimuovi Ultima Squadra    |
-               | [5] Rimuovi Squadra a Piacere |
-               | [6] Modifica Nome Squadra     |
-               | [7] Salva Modifiche           |
-               ---------------------------------
+               ------------------------------------------------
+               | [0] Termina Programma                        |
+               | [1] Stampa Menù                              |
+               | [2] Stampa Squadre del Campionato di Serie A |
+               | [3] Aggiungi Squadra al campionato           |
+               | [4] Aggiungi Squadra in Posizione a Piacere  |
+               | [5] Rimuovi Ultima Squadra del campionato    |
+               | [6] Rimuovi Squadra a Piacere dal campionato |
+               | [7] Modifica Nome Squadra                    |
+               | [8] Salva Modifiche                          |
+               ------------------------------------------------
                """);
     }
 
@@ -28,10 +29,13 @@ public class Main  {
 
         // creazione scanner e variabile selezione
         Scanner scanner = new Scanner(System.in);
+
+        // choice per scelta dal menù
         int choice;
+
         printMenu();
         // costruzione oggetto campionato
-        Campionato serieA = new Campionato("data/serieA.csv");
+        Campionato serieA = new Campionato("progetto_campionato/data/serieA.csv");
 
         do {
             System.out.print("Scelta: ");
@@ -42,19 +46,33 @@ public class Main  {
                 case 1:
                     printMenu();
                     break;
+                // stampa elenco squadre
                 case 2:
                     System.out.println(serieA);
                     break;
+                // aggiungi squadra
                 case 3:
                     serieA.addTeam(scanner);
                     break;
+                // aggiunga squadra in una posizione
                 case 4:
+                    serieA.addTeamInPos(scanner);
+                    break;
+                // rimuovi ultima squadra
+                case 5:
                     serieA.removeLastTeam();
                     break;
-                case 5:
-                    System.out.print("Numero squadra da rimuovere: ");
-                    int pos = scanner.nextInt();
-                    serieA.removeTeam(pos);
+                // rimuovi squadra a piacere
+                case 6:
+                    serieA.removeTeam(scanner);
+                    break;
+                // modifica nome squadra
+                case 7:
+                    serieA.changeTeamName(scanner);
+                    break;
+                // salva modifiche
+                case 8:
+                    serieA.saveChanges();
                     break;
                 default:
                     System.out.print("- Programma Chiuso Correttamente -");
