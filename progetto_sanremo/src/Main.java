cae914ede3d75ef8fb6096c13ab7a10d4935bfcc
luca <luca.bisognin13@gmail.com>
2024-11-14 18:21:23 +0100

    compito "sanremo" per 15/11/2024

--- progetto_sanremo/src/Main.java
+++ progetto_sanremo/src/Main.java
@@ -1,24 +1,40 @@
 /*
 Luca Bisognin - 14/11/2024
-classe Main del progetto Sanremo
+classe Main progetto Sanremo
 */
 
 import java.io.File;
-import java.util.*;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner file = new Scanner("concorrenti.txt");
-        ArrayList<String> concorrenti = new ArrayList<>();
+        try {
+            // file con i concorrenti
+            File file = new File("progetto_sanremo/data/concorrenti.txt");
 
-        /*metodo avanzato
-        ArrayList<String> concorrenti = new ArrayList<>(Arrays.asList(Objects.requireNonNull(file.list())));
-        */
+            // scanner per leggere il contenuto del file
+            Scanner scanFile = new Scanner(file);
+            // arraylist per contenere i concorrenti
+            ArrayList<String> concorrenti = new ArrayList<>();
 
-        while (file.hasNextLine()) {
-            concorrenti.add(file.nextLine());
+            /*metodo avanzato
+            ArrayList<String> concorrenti = new ArrayList<>(Arrays.asList(Objects.requireNonNull(file.list())));
+            */
+
+            // lettura file e aggiunta concorrenti all'ArrayList
+            while (scanFile.hasNextLine()) {
+                concorrenti.add(scanFile.nextLine());
+            }
+
+            // stampa elenco concorrenti
+            System.out.println(concorrenti);
+
+            scanFile.close();
+
+        } catch (FileNotFoundException e) {
+            System.out.println("File non trovato: " + e.getMessage());
         }
-
-        System.out.println(concorrenti);
     }
 }
