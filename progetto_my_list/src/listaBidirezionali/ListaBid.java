cb2f613d9d26e93270c9c9f9f60ef86bda9d626f
Luca <134801125+biso207@users.noreply.github.com>
2025-05-30 06:35:37 +0000

    lavoro 30/5/2025

--- progetto_my_list/src/listaBidirezionali/ListaBid.java
+++ progetto_my_list/src/listaBidirezionali/ListaBid.java
@@ -24,9 +24,16 @@ public class ListaBid<T> {
     // metodo per aggiungere un nodo alla lista passando il valore di un nodo da creare
     public void add(T newval) {
         NodoBid<T> newNode = new NodoBid<>(newval);
-        if (first==null) first = newNode;
-        else first.setPrev(newNode);
+        if (first == null) {
+            first = newNode;
+            last = newNode;
+        } else {
+            last.setSucc(newNode);
+            newNode.setPrev(last);
+            last = newNode;
+        }
     }
+
 
     // metodo per aggiungere un nodo in testa alla lista
     public void addHead( T newVal) {
@@ -41,6 +48,90 @@ public class ListaBid<T> {
             first = newNodo;
         }
     }
+
+    // metodo per aggiungere un nodo in coda
+    public void addTail(T val) {
+        NodoBid<T> newNode = new NodoBid<>(val);
+        if (first == null) {
+            first = newNode;
+            last = newNode;
+        } else {
+            last.setSucc(newNode);
+            newNode.setPrev(last);
+            last = newNode;
+        }
+    }
+
+    // metodo per aggiungere un nodo a una certa posizione
+    public void addInPos(T val, int pos) {
+        NodoBid<T> newNode = new NodoBid<>(val);
+        if (pos <= 0 || first == null) {
+            addHead(val);
+            return;
+        }
+
+        NodoBid<T> temp = first;
+        int i = 0;
+
+        while (temp.getSucc() != null && i < pos - 1) {
+            temp = temp.getSucc();
+            i++;
+        }
+
+        newNode.setSucc(temp.getSucc());
+        newNode.setPrev(temp);
+
+        if (temp.getSucc() != null) {
+            temp.getSucc().setPrev(newNode);
+        } else {
+            last = newNode;
+        }
+
+        temp.setSucc(newNode);
+    }
+
+    public void removeTail() {
+        if (first == null) return;
+
+        if (first == last) {
+            first = null;
+            last = null;
+        } else {
+            last = last.getPrev();
+            last.setSucc(null);
+        }
+    }
+
+    // metodo per rimuovere un nodo in una certa posizione
+    public void removeInPos(int pos) {
+        if (first == null) return;
+
+        if (pos <= 0) {
+            // rimuovi testa
+            if (first == last) {
+                first = null;
+                last = null;
+            } else {
+                first = first.getSucc();
+                first.setPrev(null);
+            }
+            return;
+        }
+
+        NodoBid<T> temp = first;
+        int i = 0;
+        while (temp != null && i < pos) {
+            temp = temp.getSucc();
+            i++;
+        }
+
+        if (temp == null) return; // posizione oltre la lunghezza
+        
+        if (temp.getSucc() != null) temp.getSucc().setPrev(temp.getPrev());
+        else last = temp.getPrev();
+
+        if (temp.getPrev() != null) temp.getPrev().setSucc(temp.getSucc());
+    }
     
     // metodo per recuperare la lunghezza della lista
     public int getLength() {
@@ -52,8 +143,6 @@ public class ListaBid<T> {
         }
         return l;
     }
-
-    // todo: addTail & addInPos e i corrispondenti remove
 
     @Override
     public String toString() {
